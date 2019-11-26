42db15dae934f740918089a87b0e93d04a7f0fd4
a <a>
2019-11-26 12:54:59 +0900

    風花雪月

---
+++ BattleShipGame/Assets/Scenes/koma.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class koma : MonoBehaviour
+{
+	int fieldID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		fieldID = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public int SetID
+    {
+        set { fieldID = value; }
+    }
+
+    public void Return()
+	{
+		Debug.Log("天ちゃんprpr");
+        Debug.Log("komaid:" + fieldID);
+    }
+}
