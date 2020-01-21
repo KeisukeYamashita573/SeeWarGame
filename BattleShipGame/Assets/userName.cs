7ae16988bfa076549260a2e446e44ef7a940623d
a <a>
2020-01-21 12:54:05 +0900

    タイトルちょっと修正

--- BattleShipGame/Assets/userName.cs
+++ BattleShipGame/Assets/userName.cs
@@ -16,26 +16,53 @@ public class userName : MonoBehaviour
     [SerializeField]
     private GameObject userMng = default;
 
+	private bool namespaceFlag;
+
     // Start is called before the first frame update
     void Start()
     {
-        login.SetActive(false);
-        newMake.SetActive(false);
-    }
+        login.SetActive(true);
+        newMake.SetActive(true);
+		nameField.gameObject.SetActive(false);
+
+		newMake.GetComponent<Button>().onClick.AddListener(CreateUer);
+		login.GetComponent<Button>().onClick.AddListener(Login);
+
+		namespaceFlag = false;
+	}
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
-        {
-            //  ユーザーネームが入力されていれば通す(空白は通さない)
-            if(!string.IsNullOrWhiteSpace(nameField.text))
-            {
-                userMng.GetComponent<user>().SetUserName(nameField.text);
-                login.SetActive(true);
-                newMake.SetActive(true);
-                nameField.gameObject.SetActive(false);
-            }
-        }
+		// 新規作成が押されたら
+		if (namespaceFlag)
+		{
+			if (Input.GetKeyDown(KeyCode.Return))
+			{
+				//  ユーザーネームが入力されていれば通す(空白は通さない)
+				if (!string.IsNullOrWhiteSpace(nameField.text))
+				{
+					userMng.GetComponent<user>().SetUserName(nameField.text);
+					login.SetActive(true);
+					newMake.SetActive(true);
+					nameField.gameObject.SetActive(false);
+					namespaceFlag = false;
+				}
+			}
+		}
     }
+
+	public void CreateUer()
+	{
+		login.SetActive(false);
+		newMake.SetActive(false);
+		nameField.gameObject.SetActive(true);
+		namespaceFlag = true;
+	}
+
+	public void Login()
+	{
+		SceneManager.LoadScene("GamePlaying");
+
+	}
 }
