27e222b30f9f96e5cf80105bd8650f14f5c96b8d
miyabi573 <cz1.7596hg[gmail.com>
2020-01-23 00:01:59 +0900

    editScripts

--- NetClient/Assets/AsynchronousClient.cs
+++ NetClient/Assets/AsynchronousClient.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Text;
+using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 public class AsynchronousClient : MonoBehaviour
 {
+    private static Socket soc;
     void Start()
     {
-        var soc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        soc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         StartAccept(soc);
     }
 
@@ -38,5 +41,42 @@ public class AsynchronousClient : MonoBehaviour
         client.Close();
 
         server.BeginAccept(new AsyncCallback(AcceptCallback), server);
+    }
+
+    private void Connect(string host, int port)
+    {
+        Debug.Log("Connect" + "ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+        IPEndPoint ipEnd = new IPEndPoint(Dns.GetHostAddresses(host)[0], port);
+        soc.Connect(ipEnd);
+        soc.BeginConnect(ipEnd, new AsyncCallback(ConnectCallback), soc);
+    }
+
+    private void ConnectCallback(IAsyncResult ar)
+    {
+        Debug.Log("ConnectCallback" + "ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+        try
+        {
+            Socket client = (Socket)ar.AsyncState;
+            client.EndConnect(ar);
+            Debug.Log("Socket Connected to " + client.RemoteEndPoint.ToString());
+            StartReceive();
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
+    }
+    private void StartReceive()
+    {
+        Debug.Log("StartReceive ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+        int len = -1;
+        
+    }
+
+    public void Send(string str)
+    {
+        Encoding enc;
+        Debug.Log("Send ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+
     }
 }
--- Server/AsynchronousServer/AsynchronousServer/AsynchronousServer.cs
+++ Server/AsynchronousServer/AsynchronousServer/AsynchronousServer.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace Server
 {
     class AsynchronousServer
     {
+        private static ManualResetEvent SocketEvent = new ManualResetEvent(false);
+        private static IPEndPoint ipEndPoint;
+        private static Socket soc;
+        private static string port = "2001";
+        private static Thread tMain;
+
         private class AsyncStateObject
         {
             public Socket Socket;
@@ -23,13 +31,41 @@ namespace Server
 
         static void Main(string[] args)
         {
-            Socket soc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            StartReceive(soc);
+            try
+            {
+                Console.WriteLine("Main ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine("Program ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+                IPAddress myIP = IPAddress.Parse("127.0.0.1");
+                ipEndPoint = new IPEndPoint(myIP, Int32.Parse(port));
+                Console.WriteLine("Init ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+                soc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                soc.Bind(ipEndPoint);
+                soc.Listen(10);
+                tMain = new Thread(new ThreadStart(Round));
+                tMain.Start();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            
         }
 
-        private static void StartReceive(Socket soc)
+        private static void Round()
+        {
+            Console.WriteLine("Round ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            while (true)
+            {
+                SocketEvent.Reset();
+                soc.BeginAccept(new AsyncCallback(StartReceive),soc);
+                SocketEvent.WaitOne();
+            }
+        }
+
+        private static void StartReceive(IAsyncResult ar)
         {
             // BeginReceiveで非同期受信を開始する
+            Console.WriteLine("StartReceive ThreadID:" + Thread.CurrentThread.ManagedThreadId);
             AsyncStateObject stateObj = new AsyncStateObject(soc);
             soc.BeginReceive(stateObj.ReceiveBuff, 0, stateObj.ReceiveBuff.Length, SocketFlags.None, new AsyncCallback(ReceiveDataCallBack), stateObj);
         }
@@ -37,6 +73,7 @@ namespace Server
         private static void ReceiveDataCallBack(IAsyncResult ar)
         {
             // 状態オブジェクト取得
+            Console.WriteLine("ReceiveDataCallback ThreadID:" + Thread.CurrentThread.ManagedThreadId);
             AsyncStateObject stateObj = (AsyncStateObject)ar.AsyncState;
             int len = 0;
             try
